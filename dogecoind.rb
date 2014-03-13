1db6ed70260accf933fbb83e7fafa56728132fdd
Marc Weistroff <marc@weistroff.net>
2014-03-13 09:00:39 +0100

    Update formula for dogecoin 1.6

--- dogecoind.rb
+++ dogecoind.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Dogecoind < Formula
     head 'https://github.com/dogecoin/dogecoin.git', :using => :git
 
-    url 'https://github.com/dogecoin/dogecoin/archive/1.5.1.tar.gz'
-    sha1 '70efea48f293c6d10281cf2251720fd8f184196d'
+    url 'https://github.com/dogecoin/dogecoin/archive/1.6.tar.gz'
+    sha1 '07cdf9b54de39f981c6f9c89a5cd0b974f266939'
 
     depends_on 'miniupnpc'
     depends_on 'openssl'
